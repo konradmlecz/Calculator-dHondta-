f420fc46a0e075fbe6abf7c703bc46a22768b61e
Konrad <konradmleczko85@gmail.com>
2019-05-22 14:57:17 +0200

    Updates

---
+++ precache-manifest.c734de84abea44be9ccf3243c2f8c7e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7caf51104c16bb98f0797ad131a133",
+    "url": "/Calculator-dHondta-/index.html"
+  },
+  {
+    "revision": "87dd4500030176e42f96",
+    "url": "/Calculator-dHondta-/static/css/main.7b6eaf95.chunk.css"
+  },
+  {
+    "revision": "5a59ff517d3b0db0a0f9",
+    "url": "/Calculator-dHondta-/static/js/2.04204285.chunk.js"
+  },
+  {
+    "revision": "87dd4500030176e42f96",
+    "url": "/Calculator-dHondta-/static/js/main.a870a32f.chunk.js"
+  },
+  {
+    "revision": "00c557eb2398379a39ab",
+    "url": "/Calculator-dHondta-/static/js/runtime~main.fd71bb4c.js"
+  }
+]);
